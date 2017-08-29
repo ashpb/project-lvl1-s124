910707d03a89a94f04eaf9348a81db615756bcea
ashpb <ashpb@me.com>
2017-08-29 11:52:00 +1000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greet = () => {
+const greet = () => {
   console.log(`Hello, ${readlineSync.question('May I have your name? ')}`);
 };
+
+export default greet;
 
